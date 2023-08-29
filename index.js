9b9578da4b2d714d6c241fcab914dc916a5207e9
Raymond Nguyen <04hng@Raymonds-MacBook.local>
2023-08-29 12:37:49 -0700

    added new folder sturctures and shape.js and shape.test to make codes look cleaner. Also switched fs module to fs.promises for async functions

--- index.js
+++ index.js
@@ -1,76 +1,84 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require('fs').promises;
+const { Circle, Triangle, Square} = require('./lib/shapes')
 // import inquirer from ('inquirer')
 // import fs from ('fs')
+// import shapes from shape.js
+// I deciided to use fs async to test my async await skills
 
 async function prompt() {
     const userInput = await inquirer.prompt([
         {
-            name: 'text',
-            message: 'enter the text (up to three characters) for the logo:',
+            name: "text",
+            message: "Enter the text (up to three characters) for the logo:",
             validate: (input) => /^[a-zA-Z]{1,3}$/.test(input),
         },
         {
             name: "textColor",
-            message: 'Enter the text color (keyword or hexadecimal number):',
+            message: "Enter the text color (keyword or hexadecimal number):",
         },
         {
-            name: 'shape',
-            type: 'list',
-            message: 'Select a shape for the logo:',
-            choices: ['circle', 'triangle', 'square'],
+            name: "shape",
+            type: "list",
+            message: "Select a shape for the logo:",
+            choices: ["Circle", "Triangle", "Square"],
         },
         {
-            name: 'shapeColor',
-            mesage: 'enter the shape color (keyword or hexadecimal number'
+            name: "shapeColor",
+            message: "Enter the shape color (keyword or hexadecimal number):",
         },
-    ])
+    ]);
+
     return userInput;
-};
+}
 
 function generateSVG(text, textColor, shape, shapeColor) {
-    let shapeMarkup = "";
+    let shapeChild;
 
-    if (shape === "circle") {
-    shapeMarkup = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
-    } else if (shape === "triangle") {
-    shapeMarkup = `<polygon points="150,20 75,180 225,180" fill="${shapeColor}" />`;
-    } else if (shape === "square") {
-    shapeMarkup = `<rect x="75" y="50" width="150" height="100" fill="${shapeColor}" />`;
+    if (shape === "Circle") {
+        shapeChild = new Circle(shapeColor);
+    } else if (shape === "Triangle") {
+        shapeChild = new Triangle(shapeColor);
+    } else if (shape === "Square") {
+    shapeChild = new Square(shapeColor);
     }
 
+    let shapeMarkup = shapeChild.generateMarkup();
+
     let svg = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+        <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
         ${shapeMarkup}
         <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" fill="${textColor}" font-size="48">${text}</text>
-    </svg>
+        </svg>
     `;
 
     return svg;
 }
 
-function saveSVGToFile(filename, svg) {
-    fs.writeFile(filename, svg, (err) => {
-        if (err) throw err;
+// used async awwait for fs module for more readable codes
+async function saveSVGToFile(filename, svg) {
+    try {
+        await fs.writeFile(filename, svg);
         console.log(`Generated ${filename}`);
-    });
+    } catch (err) {
+        console.error("An error occurred:", err);
+    }
 }
 
 async function run() {
     try {
         const userInput = await prompt();
-
         const svg = generateSVG(
             userInput.text,
             userInput.textColor,
             userInput.shape,
             userInput.shapeColor
-        );
+    );
 
-    saveSVGToFile('logo.svg', svg);
+    saveSVGToFile("logo.svg", svg);
     } catch (error) {
-    console.error('An error occurred:', error);
+        console.error("Unsucccessful", error);
     }
-}
+}   
 
 run();
